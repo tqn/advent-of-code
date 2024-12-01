12efb9c028e753fda769162bee6cc38f36f8d776
Tej Qu Nair <tej.q.nair@gmail.com>
2024-12-01 03:41:16 -0800

    parsing for day 1

--- 2024/src/Day1.hs
+++ 2024/src/Day1.hs
@@ -1,15 +1,33 @@
-module Day1 where
+{-# LANGUAGE NoImplicitPrelude #-}
 
-import Effectful (Dispatch (Dynamic), DispatchOf, Effect)
-import Effectful.TH (makeEffect)
+module Main where
+
+import Relude
+
+import Control.Arrow
+import Data.ByteString (ByteString)
+import Data.ByteString qualified as BS
+import Data.Text (Text)
+import Data.Text qualified as T
+import Data.Text.Encoding qualified as TE
+import Effectful
+import Effectful.FileSystem
+import Effectful.FileSystem.IO.ByteString qualified as EBS
+import Text.Megaparsec (Parsec, parse)
+import Text.Megaparsec.Byte qualified as Lexer
+import Text.Megaparsec.Char (newline, space, string)
+import Text.Megaparsec.Char.Lexer qualified as Lexer
+
+parseLine :: Parsec Void Text (Int, Int)
+parseLine = (,) <$> Lexer.decimal <* space <*> Lexer.decimal
+
+getInput :: (FileSystem :> es) => FilePath -> Eff es Text
+getInput path = TE.decodeUtf8Lenient <$> EBS.readFile path
 
 main :: IO ()
-main = putStrLn "Hello world!"
+main = runEff . runFileSystem $ do
+  let inputFilename = "data/day1.txt"
+  contents <- getInput inputFilename
+  let res = error . show ||| id $ parse (many $ parseLine <* newline) inputFilename contents
 
-data FileSystem :: Effect where
-    ReadFileFS :: FilePath -> FileSystem m String
-    WriteFileFS :: FilePath -> String -> FileSystem m ()
-
-makeEffect ''FileSystem
-
--- type instance DispatchOf FileSystem = Dynamic
+  print res
