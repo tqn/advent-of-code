9b8f82e83c70bd9bbf3e8f796d8957d0b5072497
Tej Qu Nair <tej.q.nair@gmail.com>
2024-12-01 04:14:00 -0800

    day 1 part 2

--- 2024/src/Day1.hs
+++ 2024/src/Day1.hs
@@ -4,9 +4,10 @@ module Main where
 
 import Relude
 
-import Control.Arrow
+import Control.Arrow hiding ((&&&))
 import Data.ByteString (ByteString)
 import Data.ByteString qualified as BS
+import Data.Map.Strict qualified as M
 import Data.Text (Text)
 import Data.Text qualified as T
 import Data.Text.Encoding qualified as TE
@@ -18,19 +19,30 @@ import Text.Megaparsec.Byte qualified as Lexer
 import Text.Megaparsec.Char (newline, space, string)
 import Text.Megaparsec.Char.Lexer qualified as Lexer
 
-process :: (Ord a, Num a) => [(a, a)] -> a
-process pairs = unzip pairs & (sort *** sort) & uncurry zip <&> dist & sum
+process1 :: (Ord a, Num a) => [(a, a)] -> a
+process1 pairs = zip (sort xs) (sort ys) <&> dist & sum
  where
+  (xs, ys) = unzip pairs
   dist (a, b) = abs (a - b)
+
+process2 :: (Ord a, Num a) => [(a, a)] -> a
+process2 pairs = xs <&> similarity & sum
+ where
+  (xs, ys) = unzip pairs
+  ycounts = foldl' (\m y -> M.insertWith (+) y 1 m) M.empty ys
+  similarity x = M.lookup x ycounts & fromMaybe 0 & (* x)
 
 parseLine :: Parsec Void Text (Int, Int)
 parseLine = (,) <$> Lexer.decimal <* space <*> Lexer.decimal
 
-program :: (FileSystem :> es) => FilePath -> Eff es Text
+program :: (FileSystem :> es) => FilePath -> Eff es (Int, Int)
 program inputFilename = do
   contents <- TE.decodeUtf8Lenient <$> EBS.readFile inputFilename
   let pairs = error . show ||| id $ parse (many $ parseLine <* newline) inputFilename contents
-  return $ show $ process pairs
+  return $ process1 &&& process2 $ pairs
 
 main :: IO ()
-main = runEff . runFileSystem $ print =<< program "data/day1.txt"
+main = runEff . runFileSystem $ do
+  (p1, p2) <- program "data/day1.txt"
+  print p1
+  print p2
